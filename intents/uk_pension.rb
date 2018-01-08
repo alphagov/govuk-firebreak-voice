39091b9416406aa0f1e5ff35b9e5c4a160ef1823
David Henry <david@decoybecoy.com>
2018-01-08 14:59:59 +0000

    get slot from request

--- intents/uk_pension.rb
+++ intents/uk_pension.rb
@@ -1,5 +1,5 @@
 intent "state_pension_available" do
-  birthday = slot_value('birthday')
+  birthday = request.slot_value('birthday')
   r = `curl https://www.gov.uk/state-pension-age/y/age/#{birthday.strftime('yyyy-mm-dd')}/male.json`
   ask(JSON.parse(r)['title'])
 end
