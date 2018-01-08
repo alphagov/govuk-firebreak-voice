c96298b47bdf829d55734b8219fc6e031ff8eb2b
David Henry <david@decoybecoy.com>
2018-01-08 15:01:25 +0000

    string not date

--- intents/uk_pension.rb
+++ intents/uk_pension.rb
@@ -1,6 +1,6 @@
 intent "state_pension_available" do
   birthday = request.slot_value('birthday')
-  r = `curl https://www.gov.uk/state-pension-age/y/age/#{birthday.strftime('yyyy-mm-dd')}/male.json`
+  r = `curl https://www.gov.uk/state-pension-age/y/age/#{birthday}/male.json`
   ask(JSON.parse(r)['title'])
 end
 
