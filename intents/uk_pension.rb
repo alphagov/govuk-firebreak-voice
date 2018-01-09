8688d4b3f70b208050ca3b6c8a8002b4faf88eba
David Henry <david@decoybecoy.com>
2018-01-09 11:52:47 +0000

    test confirmation

--- intents/uk_pension.rb
+++ intents/uk_pension.rb
@@ -1,6 +1,9 @@
 intent "state_pension_available" do
   birthday = request.slot_value('birthday')
   gender = request.slot_value('gender')
+  puts '*********'
+  puts request.instance_variable_get(:@request)['request']
+  puts '*********'
   if request.instance_variable_get(:@request)['request']['dialogState'] == 'COMPLETE'
     r = `curl https://www.gov.uk/state-pension-age/y/age/#{birthday}/#{gender}.json`
     tell(JSON.parse(r)['title'])
