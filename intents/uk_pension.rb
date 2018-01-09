e26239d0eef5473803163020f6230eb826a6b603
David Henry <david@decoybecoy.com>
2018-01-09 15:58:11 +0000

    more complicated flows

--- intents/uk_pension.rb
+++ intents/uk_pension.rb
@@ -1,13 +1,47 @@
+require 'date'
+
+class PensionAge
+  FIELDS = %w{birthday gender}
+  def initialize(request)
+    @request = request
+  end
+
+  def birthday
+    @request['slots']['birthday']['value']
+  end
+
+  def birthday?
+    birthday && birthday =~ /\A\d\d\d\d-\d?\d-\d?\d\Z/ && birthday !~ /\A#{Date.today.year}/
+  end
+
+  def gender
+    @request['slots']['birthday']['value']
+  end
+
+  def valid?
+    has_all_fields? && !pending_dialog?
+  end
+
+  def has_all_fields?
+    gender && birthday?
+  end
+
+  def pending_dialog?
+    @request['dialogState'] && @request['dialogState'] != 'COMPLETED'
+  end
+end
+
+
+
 intent "state_pension_available" do
-  birthday = request.slot_value('birthday')
-  gender = request.slot_value('gender')
-  if request.instance_variable_get(:@request)['request']['dialogState'] == 'COMPLETED'
-    r = `curl https://www.gov.uk/state-pension-age/y/age/#{birthday}/#{gender}.json`
+  pension_age = PensionAge.new(request.instance_variable_get(:@request)['request'])
+
+  if pension_age.valid?
+    r = `curl https://www.gov.uk/state-pension-age/y/age/#{pension_age.birthday}/#{pension_age.gender}.json`
     tell(JSON.parse(r)['title'])
+  elsif pension_age.birthday && !pension_age.birthday?
+    dialog_elicit("Please enter your full birthday including month and year", 'birthday')
   else
-    puts '--------'
-    puts "No birthday"
-    puts '--------'
     dialog_delegate
   end
 end
