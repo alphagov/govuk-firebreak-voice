b46a01bd026a8bbc6c7029cf7124d148fb07fa88
David Henry <david@decoybecoy.com>
2018-01-09 09:37:24 +0000

    Use new dialoag method

--- intents/uk_pension.rb
+++ intents/uk_pension.rb
@@ -2,12 +2,12 @@ intent "state_pension_available" do
   birthday = request.slot_value('birthday')
   if birthday && birthday != ''
     r = `curl https://www.gov.uk/state-pension-age/y/age/#{birthday}/male.json`
-    ask(JSON.parse(r)['title'])
+    dialog_delegate(JSON.parse(r)['title'])
   else
     puts '--------'
     puts "No birthday"
     puts '--------'
-    respond
+    dialog_delegate
   end
 end
 
