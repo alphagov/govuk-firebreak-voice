b1b1abc7e0fe7110c16c8690957f5c431d5f0ba7
David Henry <david@decoybecoy.com>
2018-01-10 13:03:57 +0000

    Try alternative approach

--- intents/uk_pension.rb
+++ intents/uk_pension.rb
@@ -6,7 +6,7 @@ class PensionAge
   end
 
   def birthday
-    @request['intent']['slots']['birthday']['value']
+    @request['intent']['slots']['birthday']['value'] unless ['intent']['slots']['gender']['confirmationStatus'] == 'DENIED'
   end
 
   def birthday?
@@ -14,7 +14,7 @@ class PensionAge
   end
 
   def gender
-    @request['intent']['slots']['gender']['value']
+    @request['intent']['slots']['gender']['value'] unless ['intent']['slots']['gender']['confirmationStatus'] == 'DENIED'
   end
 
   def valid?
@@ -51,4 +51,97 @@ end
 
 intent "SessionEndedRequest" do
   respond
+end
+
+
+intent "getDate" do
+  session = load_session(request)
+  if session.can?(:get_date)
+    session.perform(:add_date, field: :date)
+    if session.valid?
+      confirm_action(session)
+    else
+      build_reask_action(session)
+    end
+  else
+    build_action_not_alllowed(session)
+  end
+end
+
+intent "getNumber" do
+  session = load_session(request)
+  if session.can?(:get_number)
+    session.perform(:add_number, field: :number)
+    if session.ready_for_confirmation?
+      build_confirm_action(session)
+    else
+      build_ask_action(session)
+    end
+  else
+    build_action_not_alllowed(session)
+  end
+end
+
+intent "getConfirmation" do
+  session = load_session(request)
+  if session.can?(:get_confirmation)
+    session.perform(:get_confirmation, field: :confirmation)
+
+    build_ask_action(session)
+  else
+    build_action_not_alllowed(session)
+  end
+end
+
+intent "pension_age" do
+  session = load_session(request)
+  session.perform(:add_date, field: :birthday)
+  build_ask_action(session)
+end
+
+def load_session(request)
+  Session.new(
+    last_request: request.session_attributes('last_request'),
+    allowed_actions: request.session_attributes('allowed_actions'),
+  )
+end
+
+def build_ask_action(session)
+  if session.complete?
+    ask(*session.ask_details)
+  else
+    build_answer(session)
+  end
+end
+
+class Session
+  def initialize(last_request:, allowed_actions:)
+    @last_request = last_request
+    @allowed_actions = allowed_actions
+  end
+
+  def complete?
+    false # TODO: add this
+  end
+
+  def next_action
+    :birthday # TODO: calc this
+  end
+
+  def ask_details
+    case next_action
+    when :birthday
+      question = "Ok, what’s your date of birth?"
+      allowed_actions = %(getDate getNumber)
+    end
+
+    [question, session_attributes: { last_request: question, allowed_actions: allowed_actions }]
+  end
+
+  def attributes
+    {
+      last_request: @last_request,
+      allowed_actions: @allowed_actions
+    }
+  end
 end
