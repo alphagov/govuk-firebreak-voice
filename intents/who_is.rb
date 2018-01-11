9b366cd1c182498354a2a90526e24b135a3f9672
David Henry <david@decoybecoy.com>
2018-01-11 15:53:59 +0000

    fix nokkogiri

--- intents/who_is.rb
+++ intents/who_is.rb
@@ -2,7 +2,7 @@ require 'open-uri'
 require 'nokogiri'
 intent 'minister_for_cabinet_office' do
   html = open('https://www.gov.uk/government/ministers/minister-for-the-cabinet-office').read
-  title = Nokogiri.parse(html).css('.current-role-holder h1').first.value
+  title = Nokogiri.parse(html).css('.current-role-holder h1').first.text.strip
   title.gsub!(/^Current role holder: /, '')
 
   tell("#{title} is the minister for Cabinet Office")
