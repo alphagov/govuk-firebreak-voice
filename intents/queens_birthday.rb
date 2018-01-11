0e0655f40dfe7ac0dacc4c16104e0aaef0b9412b
David Henry <david@decoybecoy.com>
2018-01-11 15:29:02 +0000

    Fix date calc

--- intents/queens_birthday.rb
+++ intents/queens_birthday.rb
@@ -3,7 +3,7 @@ require 'date'
 intent 'queens_birthday' do
   official_date = Date.new(Date.today.year, 6, 1)
 
-  while official_date.mday != 5
+  while official_date.wday != 5
     official_date += 1 # find the first saturday
   end
   official_date += 7 # second saturday
@@ -18,7 +18,7 @@ intent 'queens_birthday' do
                end
   tell(<<~MSG
       The Queen’s birthday is on the 21st of April. She also has an official birthday 
-      and public celebration in June. This year it #{will_be_was} on #{official_date.strftime("%A the %-d#{ordinality} of %B")}[Saturday the 9th of June].
+      and public celebration in June. This year it #{will_be_was} on #{official_date.strftime("%A the %-d#{ordinality} of %B")}.
     MSG
   )
 end
