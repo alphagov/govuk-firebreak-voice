d8fd6633548cc5d20be8fad7fb78a271d37ca097
David Henry <david@decoybecoy.com>
2018-01-12 14:31:46 +0000

    Add readme and remove old code

---
+++ README.md
@@ -0,0 +1,16 @@
+# Alexa voice demo application
+
+This repo contains:
+
+* backend implementation using sinatra  
+* JSON confirmation file for the Alexa application.
+
+This implements the following example questions with answers:
+
+* When is the Queens birthday
+* Who is the minister for the cabinet office
+* Can I get Universal Credit
+* When can I get state pension
+
+
+
--- intents/uk_pension.rb
+++ intents/uk_pension.rb
@@ -1,50 +1,5 @@
 require 'date'
 
-class PensionAge
-  def initialize(request)
-    @request = request
-  end
-
-  def birthday
-    @request['intent']['slots']['birthday']['value'] unless ['intent']['slots']['gender']['confirmationStatus'] == 'DENIED'
-  end
-
-  def birthday?
-    birthday && birthday =~ /\A\d\d\d\d-\d?\d-\d?\d\Z/ && birthday !~ /\A#{Date.today.year}/
-  end
-
-  def gender
-    @request['intent']['slots']['gender']['value'] unless ['intent']['slots']['gender']['confirmationStatus'] == 'DENIED'
-  end
-
-  def valid?
-    has_all_fields? && !pending_dialog?
-  end
-
-  def has_all_fields?
-    gender && birthday && birthday?
-  end
-
-  def pending_dialog?
-    @request['dialogState'] && @request['dialogState'] != 'COMPLETED'
-  end
-end
-
-intent "simple_state_pension" do
-  pension_age = PensionAge.new(request.instance_variable_get(:@request)['request'])
-
-  if pension_age.valid?
-    r = `curl https://www.gov.uk/state-pension-age/y/age/#{pension_age.birthday}/#{pension_age.gender}.json`
-    tell(JSON.parse(r)['title'])
-  elsif pension_age.birthday && !pension_age.birthday?
-    puts "invalid birthday"
-    dialog_elicit("Please enter your full birthday including month and year", 'birthday')
-  else
-    puts "delegate"
-    dialog_delegate
-  end
-end
-
 intent "LaunchRequest" do
   respond(
     <<~MSG
